2a95bf66d98dde78fee0be58754f3842f66fbf0c
Marcelo Santos <rocha.mer21@gmail.com>
2025-05-25 20:07:52 -0300

    feat: Adiciona condição para idosos
    Adiciona um bloco `elif` para verificar se a idade é maior ou igual a 65 e imprime "Você é um idoso".

--- 08-condicionais.py
+++ 08-condicionais.py
@@ -1,8 +1,10 @@
-idade = 18
+idade = 65
 
-if idade >= 18:
+if idade >= 18 and idade < 65:
     print("Você é um adulto")
+elif idade >= 65:
+    print("Você é um idoso")
 else:
     print("Você é menor de idade")
 
-#Siga o modelo acima e cria uma nova expressão condicional
+# Siga o modelo acima e cria uma nova expressão condicional
