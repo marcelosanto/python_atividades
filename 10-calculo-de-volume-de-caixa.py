92858be20ad5a8fce862418bfabecdc1dbf4f356
Marcelo Santos <rocha.mer21@gmail.com>
2025-05-25 20:16:48 -0300

    feat: Adiciona script para cálculo do volume de uma caixa retangular
    Adiciona um novo script em Python que calcula o volume de uma caixa.

---
+++ 10-calculo-de-volume-de-caixa.py
@@ -0,0 +1,23 @@
+# == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+# 3. CALCULO DO VOLUME DE UMA CAIXA RETANGULAR
+# == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+# Objetivo:
+# Multiplicar tres valores correspondentes as dimensoes de uma caixa e calcular seu volume.
+
+# Instrucoes:
+# - Escreva um programa que armazene os valores de comprimento, largura e altura de uma caixa.
+# - Calcule o volume e exiba o resultado.
+
+# Explicacao:
+# - O volume de uma caixa retangular e calculado multiplicando-se as tres dimensoes.
+# - Essa e uma aplicacao pratica de multiplicacao entre variaveis.
+# - O resultado final deve ser exibido com clareza para o usuario.
+
+# 55 cm (comprimento) x 40 cm (largura) x 30 cm (altura) = 66000 cm3
+
+comprimento = 55
+largura = 40
+altura = 30
+
+print(f"O volume da caixa é: {comprimento*largura*altura}cm³")
