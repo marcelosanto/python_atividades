763fb1ac39dc34f0e308f7cce20cb2c599e3ac4d
Marcelo Santos <rocha.mer21@gmail.com>
2025-05-25 20:06:09 -0300

    feat: Adiciona lógica de comparação entre x e y
    Adiciona um bloco if/else para comparar os valores de x e y e imprime uma mensagem informando qual é o maior.

--- 07-comparacoes.py
+++ 07-comparacoes.py
@@ -1,5 +1,9 @@
 x = 10
 y = 15
-print(x > y, x < y, x == y, x != y)
 
-#Compare dois números e exiba uma frase na tela de acordo com a comparação escolhida
+if x > y:
+    print("x é maior que y")
+else:
+    print("y é maior que x")
+
+# Compare dois números e exiba uma frase na tela de acordo com a comparação escolhida
