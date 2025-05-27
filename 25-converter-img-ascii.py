dd064d4aee84172957a3a2f4fcb9d45b8216961f
Marcelo Santos <rocha.mer21@gmail.com>
2025-05-27 20:40:25 -0300

    feat: Adiciona script para converter imagem URL em ASCII art

---
+++ 25-converter-img-ascii.py
@@ -0,0 +1,45 @@
+import requests
+import random
+from PIL import Image
+from io import BytesIO
+import time
+
+ASCII_CHARS = "@%#*+=-:.FlLF1 "
+
+
+def redimensionar(imagem, nova_largura=50):
+    largura, altura = imagem.size
+    proporcao = altura / largura
+    nova_altura = int(nova_largura * proporcao * 0.55)
+    return imagem.resize((nova_largura, nova_altura))
+
+
+def converter_ascii(imagem):
+    imagem = imagem.convert("L")  # Escala de cinza
+    pixels = imagem.getdata()
+    ascii_str = "".join(
+        ASCII_CHARS[pixel * (len(ASCII_CHARS) - 1) // 255] for pixel in pixels)
+
+    return ascii_str
+
+
+def imagem_url_para_ascii(url, largura=50):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        imagem = Image.open(BytesIO(response.content))
+    except Exception as e:
+        print(f"Erro ao obter a imagem: {e}")
+        return
+
+    imagem = redimensionar(imagem, largura)
+    ascii_str = converter_ascii(imagem)
+
+    ascii_img = "\n".join(
+        ascii_str[i:i+imagem.width] for i in range(0, len(ascii_str), imagem.width)
+    )
+    print(ascii_img)
+
+
+imagem_url_para_ascii(
+    f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{random.randint(1, 151)}.png")
